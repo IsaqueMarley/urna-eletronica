f9cc9226688c0ea48b03797827777a4533f5c338
andersondk12 <131102035+andersondk12@users.noreply.github.com>
2023-04-24 15:57:02 -0300

    Controller and Model
    Classes de modelo e controle da interface

---
+++ Controller/Helper/LoginHelper.java
@@ -0,0 +1,4 @@
+package Controller.Helper;
+
+public class LoginHelper {
+}
---
+++ Model/Eleitor.java
@@ -0,0 +1,40 @@
+package Model;
+
+public class Eleitor extends Pessoa{
+    private String rg;
+    private String cpf;
+    private String cep;
+    private String senha;
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
